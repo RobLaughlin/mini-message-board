9c6e9ccd7f6f17a810f33e09b586baaa041dab6c
Rob Laughlin <waterfallkicks@gmail.com>
2025-06-04 15:10:40 -0700

    Implement basic post message form using tailwind

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
-import express, { Request, Response } from "express";
-import CONFIG from "./config/config";
+import express from "express";
+import "dotenv/config";
+import path from "path";
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
 
-app.listen(CONFIG.PORT, () => {
-    console.log(`Listening on port: ${CONFIG.PORT}...`);
+app.get("/", (_, res) => {
+    res.render("index");
+    // res.send("YOO");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
 });
